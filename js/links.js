bc75579765be180d3ab560d3f67a2ede0d724622
Scholl Sebastian <it170167@EDU.HTL-LEONDING.AC.AT>
2018-12-12 13:36:40 +0100

    added elements to links-array

--- js/links.js
+++ js/links.js
@@ -1,1 +1,23 @@
 let links = new Array();
+
+links['githubPages'] = [
+    'GameOfThrones',
+    'slideshow',
+    'notenliste',
+    'schollsebastian.github.io',
+    'kurs'
+]
+
+links['repositories'] = [
+    'schollsebastian.github.io',
+    'notenliste',
+    'kurs',
+    'Fizz-Buzz',
+    'Dame',
+    'Instanzenzaehler',
+    'GameOfThrones',
+    'CodeDecode',
+    'slideshow',
+    'BastiBall',
+    'BastiBot'
+]
