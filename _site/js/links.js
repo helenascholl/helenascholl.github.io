6fa5b93ca969b3a00cdfb445bfd6b0b9836ad9f8
schollsebastian <scholl.sebastian@gmx.at>
2019-03-12 00:37:07 +0100

    jekyll files

---
+++ _site/js/links.js
@@ -0,0 +1,22 @@
+let links = [];
+
+links['githubPages'] = [
+    'GameOfThrones',
+    'slideshow',
+    'notenliste',
+    'kurs',
+    'Browsergame',
+    'FormVerification',
+    'ImageManipulation'
+]
+
+links['cooperations'] = [
+    {
+        name: 'starkalukas',
+        link: 'https://starkalukas.github.io'
+    },
+    {
+        name: 'PhilippEdlinger',
+        link: 'https://philippedlinger.github.io'
+    }
+]
