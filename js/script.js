23577628fcd02e775014c7988cd984c4a9083625
Sebastian Scholl <scholl.sebastian@gmx.at>
2019-11-30 18:30:58 +0100

    add GitHUb API and remove static approach

--- js/script.js
+++ js/script.js
@@ -1,63 +1,73 @@
-window.addEventListener('load', () => {
-    let githubPages = links['githubPages'];
-    let repositories = links['repositories'];
-    let cooperations = links['cooperations'];
+let repositories = [];
 
-    githubPages.sort();
-    repositories.sort();
-
-    for (let githubPage of githubPages) {
-        let a = document.createElement('a');
-        let div = document.createElement('div');
-
-        div.textContent = githubPage;
-
-        a.href = `https://schollsebastian.github.io/${githubPage}/`;
-        a.target = '_blank';
-        a.appendChild(div);
-
-        document.getElementById('github-pages').appendChild(a);
-    }
-
-    for (let repository of repositories) {
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        let div = document.createElement('div');
-        let a = document.createElement('a');
-        let form = document.createElement('form');
-        let input = document.createElement('input');
-
-        div.textContent = repository;
-
-        a.href = `https://github.com/schollsebastian/${repository}`;
-        a.target = '_blank';
-        a.appendChild(div);
-
-        input.type = 'text';
-        input.value = `https://github.com/schollsebastian/${repository}.git`;
-
-        form.appendChild(input);
-
-        td1.appendChild(a);
-        td2.appendChild(form);
-
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-
-        document.getElementById('table').appendChild(tr);
-    }
-    
-    for (let cooperation of cooperations) {
-        let a = document.createElement('a');
-        let div = document.createElement('div');
-
-        div.textContent = cooperation.name;
-
-        a.href = cooperation.link;
-        a.target = '_blank';
-        a.appendChild(div);
-
-        document.getElementById('cooperations').appendChild(a);
-    }
+addEventListener('load', () => {
+    xmlHttpRequest('GET', 'https://api.github.com/users/schollsebastian/repos?sort=created', handleResponse)
 });
+
+function showRepositories() {
+    console.log(repositories);
+
+    for (const repository of repositories) {
+        let div = document.createElement('div');
+        let htmlUrl = document.createElement('a');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let cloneUrl = document.createElement('input');
+        let button = document.createElement('button');
+
+        htmlUrl.href = repository.html_url;
+        h2.textContent = repository.fork ? repository.full_name : repository.name;
+        p.textContent = repository.description;
+        cloneUrl.value = repository.clone_url;
+        button.textContent = 'Copy';
+
+        button.addEventListener('click', () => {
+            cloneUrl.select();
+            cloneUrl.setSelectionRange(0, 500);
+            document.execCommand('copy');
+
+            button.textContent = 'Copied!'
+            setTimeout(() => {
+                button.textContent = 'Copy'
+            }, 2000);
+        });
+
+        htmlUrl.appendChild(h2);
+        div.appendChild(htmlUrl);
+        div.appendChild(p);
+        div.appendChild(cloneUrl);
+        div.appendChild(button);
+
+        if (repository.has_pages) {
+            let pagesUrl = document.createElement('a');
+
+            pagesUrl.textContent = 'GitHub Page';
+            pagesUrl.href = `https://schollsebastian.github.io/${repository.name}`;
+            pagesUrl.classList.add('pagesUrl');
+
+            div.appendChild(pagesUrl);
+        }
+
+        document.getElementById('repositories').appendChild(div);
+    }
+}
+
+function handleResponse(load) {
+    let link = load.target.getResponseHeader('link');
+    
+    repositories.push(... JSON.parse(load.target.responseText));
+
+    if (link.includes('next')) {
+        let url = link.split('>')[0].substring(1);
+        xmlHttpRequest('GET', url, handleResponse);
+    } else {
+        showRepositories();
+    }
+}
+
+function xmlHttpRequest(method, url, callback, body = null) {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', callback);
+    request.open(method, url);
+    request.send(JSON.stringify(body));
+}
