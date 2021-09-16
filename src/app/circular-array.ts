956767f5fddb9e9126240c370f7eb974294c4b01
schollsebastian <scholl.sebastian@gmx.at>
2021-09-16 09:11:27 +0200

    Create CircularArray class

---
+++ src/app/circular-array.ts
@@ -0,0 +1,17 @@
+export class CircularArray<T> {
+
+  private readonly values: T[];
+
+  constructor(values: T[] = []) {
+    this.values = values;
+  }
+
+  public push(... items: T[]): void {
+    this.values.push(... items);
+  }
+
+  public get(index: number): T {
+    return this.values[(index % this.values.length + this.values.length) % this.values.length];
+  }
+
+}
